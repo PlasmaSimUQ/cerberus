4472907ab1f391c3fa6030c8f33baabe21c49d2f
Shahzeb Imran <s.imran@tuta.io>
2024-07-30 18:18:46 +1000

    Update build_eilmer.sh
    Add escaped quotes so paths with weird characters are output correctly in the env file

--- build_eilmer.sh
+++ build_eilmer.sh
@@ -24,8 +24,8 @@ cp -r ${EILMER_SRC}/build/* ${EILMER_HOME}/
 cp -r ${EILMER_SRC}/extern/lua-5.4.3/install/* ${EILMER_HOME}/
 
 echo "Writing Eilmer environment variables to ./env"
-echo "export DGD=${EILMER_HOME}" > env
-echo "export DGD_REPO=${EILMER_SRC}" >> env
-echo "export PATH=$PATH:${EILMER_HOME}/bin" >> env
-echo "export DGD_LUA_PATH=${EILMER_HOME}/lib/?.lua" >> env
-echo "export DGD_LUA_CPATH=${EILMER_HOME}/lib/?.so" >> env
+echo "export DGD=\"${EILMER_HOME}\"" > env
+echo "export DGD_REPO=\"${EILMER_SRC}\"" >> env
+echo "export PATH=\"$PATH:${EILMER_HOME}/bin\"" >> env
+echo "export DGD_LUA_PATH=\"${EILMER_HOME}/lib/?.lua\"" >> env
+echo "export DGD_LUA_CPATH=\"${EILMER_HOME}/lib/?.so\"" >> env
